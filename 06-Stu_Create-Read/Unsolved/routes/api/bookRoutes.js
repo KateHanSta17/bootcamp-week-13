4b1b88ba39ce830998b19c60260146eb00a34983
Hank Star <kate.stapley@hotmail.com>
2024-08-22 18:56:41 +1000

    thursday folders added

--- 06-Stu_Create-Read/Unsolved/routes/api/bookRoutes.js
+++ 06-Stu_Create-Read/Unsolved/routes/api/bookRoutes.js
@@ -2,24 +2,30 @@ const router = require('express').Router();
 const Book = require('../../models/Book');
 
 // TODO: Add a comment describing the purpose of this route
+// this route is getting all the books and returning them as json
 router.get('/', (req, res) => {
   // TODO: Add a comment describing the functionality of this method
+  // The functionality of this method is to find all the books in the database and return them as json. 
   Book.findAll().then((bookData) => {
     res.json(bookData);
   });
 });
 
 // TODO: Add a comment describing the purpose of this route
+// This route is getting all the books that are paperbacks and returning them as json
 router.get('/paperbacks', (req, res) => {
   Book.findAll({
     // TODO: Add a comment describing the functionality of this property
+    // This property is ordering the books by title
     order: ['title'],
     // TODO: Add a comment describing the functionality of this property
+    // This property is filtering the books that are paperbacks
     where: {
       is_paperback: true
     },
     attributes: {
       // TODO: Add a comment describing the functionality of this property
+      // This property is excluding the is_paperback and edition columns
       exclude: ['is_paperback', 'edition']
     }
   }).then((bookData) => {
@@ -28,8 +34,11 @@ router.get('/paperbacks', (req, res) => {
 });
 
 // TODO: Add a comment describing the purpose of this route
+// The purpose of this route is to get a specific book by its id and return it as json. 
+// This route is getting a specific book by its id and returning it as json
 router.get('/:id', (req, res) => {
   // TODO: Add a comment describing the functionality of this method
+  // The functionality of this method is to find a specific book by its id and return it as json.
   Book.findByPk(req.params.id).then((bookData) => {
     res.json(bookData);
   });
