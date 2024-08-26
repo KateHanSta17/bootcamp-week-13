c8800e2816e3d58268d3d5e8b6932997bd36b013
Hank Star <kate.stapley@hotmail.com>
2024-08-26 04:35:01 -0400

    files added

--- 12-Stu_RESTful-Routes/Unsolved/routes/api/userRoutes.js
+++ 12-Stu_RESTful-Routes/Unsolved/routes/api/userRoutes.js
@@ -5,16 +5,47 @@ const User = require('../../models/User');
 // TODO: Use try/catch to catch errors
 // TODO: Return the appropriate HTTP status codes
 
-// GET a user
+// GET a user by ID
 router.get('/:id', async (req, res) => {
-});
-
-// UPDATE a user
+    try {
+      const user = await User.findByPk(req.params.id);
+      if (!user) {
+        return res.status(404).json({ message: 'No user with this id!' });
+      }
+      res.status(200).json(user);
+    } catch (err) {
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  });
+  
+// UPDATE a user by ID
 router.put('/:id', async (req, res) => {
-});
-
-// DELETE a user
+    try {
+      const [updated] = await User.update(req.body, {
+        where: { id: req.params.id }
+      });
+      if (!updated) {
+        return res.status(404).json({ message: 'No user with this id!' });
+      }
+      const updatedUser = await User.findByPk(req.params.id);
+      res.status(200).json(updatedUser);
+    } catch (err) {
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  });
+  
+// DELETE a user by ID
 router.delete('/:id', async (req, res) => {
-});
-
-module.exports = router;
+    try {
+      const deleted = await User.destroy({
+        where: { id: req.params.id }
+      });
+      if (!deleted) {
+        return res.status(404).json({ message: 'No user with this id!' });
+      }
+      res.status(200).json({ message: 'User deleted successfully!' });
+    } catch (err) {
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  });
+  
